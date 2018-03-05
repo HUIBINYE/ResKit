3cd2cf153a50c4c835098153858282cbaec35b36
huibin <yuanhuibin@putao.com>
2018-03-05 22:48:55 +0800

    0.0.0.2 add build asset bundle function

---
+++ Assets/QFramework/ResKit/Editor/BuildAssetBundles.cs
@@ -0,0 +1,25 @@
+﻿/****************************************************************************
+ * Copyright (c) 2018 yuanhuibin@putao.com
+ ****************************************************************************/
+
+using System.IO;
+using UnityEditor;
+
+/// <summary>
+/// 打包AssetBundles
+/// from:https://docs.unity3d.com/Manual/AssetBundles-Workflow.html(内容很新，很受用)
+/// </summary>
+public class BuildAssetBundles
+{
+	[MenuItem("Assets/Build AssetBundles")]
+	static void BuildAllAssetBundles()
+	{
+		string assetBundleDirectory = "Assets/AssetBundles";
+		if (!Directory.Exists(assetBundleDirectory))
+		{
+			Directory.CreateDirectory(assetBundleDirectory);
+		}
+
+		BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneOSXIntel);
+	}
+}
